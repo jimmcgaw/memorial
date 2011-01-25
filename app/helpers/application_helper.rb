6f30d0b5f643e865d8a352efce5afd037a6df47c
Jim McGaw <jpmcgaw@gmail.com>
2011-01-24 22:57:30 -0800

    Change alt tag of logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   
   def logo
-    image_tag("logo1.png", :width => 102, :height => 52, :alt => "Worthy")
+    image_tag("logo1.png", :width => 102, :height => 52, :alt => "Memorial")
   end
 end
